e1c6307e3277f48c6e7eab4b25b99e2200892853
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2025-02-05 23:23:58 +0100

    import controllers, create endpoints

---
+++ routes/AdminRoutes.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { CreateVendor, GetVendors, GetVendorById } from '../controllers';
+
+const router = express.Router();
+
+router.post('/vendor', CreateVendor);
+
+router.get('/vendor', GetVendors);
+router.get('/vendor/:id', GetVendorById);
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+
+	res.json({ message: 'Hello from Admin!' });
+});
+
+
+export { router as AdminRoute };
